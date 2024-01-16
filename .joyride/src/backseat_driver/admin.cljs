8879ace94870b532cd2e8bbb2b5403b82e14b0b8
Peter Strömberg <pez@pezius.com>
2024-01-16 23:49:11 +0100

    Add admin for purging threads

--- .joyride/src/backseat_driver/admin.cljs
+++ .joyride/src/backseat_driver/admin.cljs
@@ -10,23 +10,44 @@
                             "Openai-Organization" conf/org,
                             "OpenaI-Beta" "assistants=v1"})
 
-(defn- purge-assistants-ids! [ids]
+(defn- purge-assistants-with-ids! [ids]
   (if (not-empty ids)
     (let [a-id (first ids)]
       (println (count ids) "Deleting: " a-id "\n")
       (p/let [_ (openai.beta.assistants.del a-id)]
-        (js/setTimeout #(purge-assistants-ids! (rest ids)) 100)))
+        (js/setTimeout #(purge-assistants-with-ids! (rest ids)) 100)))
     (println "Done")))
 
+;; Probably not needed more now that I have stopped creating
+;; tons of assistants
 (defn purge-assistants! [limit]
   (let [url "https://api.openai.com/v1/assistants"
         params #js {"limit" limit}]
     (p/let [resp (axios/get url #js {:headers headers :params params})
             resp-clj (js->clj resp :keywordize-keys true)
             ids (map #(:id %) (:data (:data resp-clj)))]
-      (println "Purge started:" (count ids) "assistants to delete\n")
-      (purge-assistants-ids! ids))))
+      (println "Purge started:" (dec (count ids)) "assistants to delete\n")
+      ;; rest so that we don't delete the latest assistant
+      (purge-assistants-with-ids! (rest ids)))))
+
+
+(defn- purge-threads-with-ids! [ids]
+  (if (not-empty ids)
+    (let [thread-id (first ids)]
+      (println (count ids) "Deleting: " thread-id "\n")
+      (p/let [_ (openai.beta.threads.del thread-id)]
+        (js/setTimeout #(purge-threads-with-ids! (rest ids)) 100)))
+    (println "Done")))
+
+(defn purge-threads! [limit]
+  (let [url "https://api.openai.com/v1/threads"
+        params #js {"limit" limit}]
+    (p/let [resp (axios/get url #js {:headers headers :params params})
+            resp-clj (js->clj resp :keywordize-keys true)
+            ids (drop 10 (map #(:id %) (:data (:data resp-clj))))]
+      (println "Purge started:" (count ids) "threads to delete\n")
+      (purge-threads-with-ids! ids))))
 
 (comment
-  (purge-assistants! 20)
+  (purge-threads! 10)
   :rcf)
