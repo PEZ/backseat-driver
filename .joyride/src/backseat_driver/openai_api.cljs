40d609dcdfdcd003539e7775a713c785694b7a1a
Peter Strömberg <pez@pezius.com>
2024-01-21 12:12:10 +0100

    Avoid openai api to be attempted in CI/test

--- .joyride/src/backseat_driver/openai_api.cljs
+++ .joyride/src/backseat_driver/openai_api.cljs
@@ -1,4 +1,8 @@
 (ns backseat-driver.openai-api
   (:require ["openai" :as openai]))
 
-(def openai (openai/OpenAI.))
+(def openai (if js/process.env.JOYRIDE_HEADLESS
+              (do
+                (println "HEADLESS TEST RUN: Providing a dummy openai object")
+                #js {})
+              (openai/OpenAI.)))
